a8cbd0bdfe36c1f46d74bef6b9457fd584bddd86
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-05 18:03:32 +0530

    Rotate Image Question 90 degree

---
+++ ProblemSolving/LogicalThinking/RotateImage.java
@@ -0,0 +1,50 @@
+package LogicalThinking;
+
+import java.util.*;
+public class RotateImage {
+    public static void main(String argp[]) {
+        Scanner sc = new Scanner(System.in);
+        // int N = sc.nextInt();
+        // int[][] matrix = new int[N][N];
+        // for(int i=0;i<N;i++) {
+        //     for(int j=0;j<N;j++) {
+        //         matrix[i][j] = sc.nextInt();
+        //     }
+        // }
+
+        int N = 4;
+        int[][] matrix = new int[][] {{1, 2, 3, 4},
+                                    {5, 6, 7, 8},
+                                    {9, 10, 11, 12},
+                                {13, 14, 15, 16}};
+
+        for(int i=0;i<N;i++) {
+            for(int j=i+1;j<N;j++) {
+                int temp = matrix[i][j];
+                // System.out.println(temp);
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+
+            }
+        }
+        
+        for(int i=0;i<N;i++) {
+            int k= N-1;
+            for(int j=0;j<N/2;j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+                k--;
+            }
+        }
+        main(matrix, N);
+
+
+        sc.close();
+    }
+
+    public static void main(int[][] matrix, int N) {
+        for(int i=0;i<N;i++)
+            System.out.println(Arrays.toString(matrix[i]));
+    }
+}
